7d8a5258f385a0efb934b0eb7e81c297c9b6c693
Stefan <git@stefn.co.uk>
2025-05-22 22:34:49 +0000

    Update example config

--- esp32-micropython/config.py
+++ esp32-micropython/config.py
@@ -1,4 +1,4 @@
-DEVICE_ID = "dSF3nNxbq4Ny"
+DEVICE_ID = "A1B2C3D4E5F6"
 
 # Board stuff
 OPTO_PIN = 18  # Using the same pin that was used for relay
@@ -7,7 +7,7 @@ LED_PIN = 2  # Onboard LED on most ESP32 boards
 
 # Server stuff (probably doesn't need changing)
 POST_INTERVAL = 2000 # 2 seconds between attempts
-SERVER_URL = "https://esp32.chobble.com"
+SERVER_URL = "https://example.com"
 API_PATH = "/api/v1/credits/claim"
 
 # Known wifi stuff
