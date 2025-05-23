d49a89a7e935b7c201820216dd1a981bceba90d1
Stefan <git@stefn.co.uk>
2025-05-23 05:31:22 +0000

    Remove solid queue models

--- app/models/solid_queue/ready_execution.rb
+++
@@ -1,7 +0,0 @@
-module SolidQueue
-  class ReadyExecution < ApplicationRecord
-    self.table_name = "solid_queue_ready_executions"
-
-    belongs_to :job, class_name: "SolidQueue::Job", foreign_key: :job_id
-  end
-end
