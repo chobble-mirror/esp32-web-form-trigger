edb0a215d267c1df646f229b7a2a151751da9dca
Stefan <git@stefn.co.uk>
2025-05-23 05:10:02 +0000

    Fix mime types

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,5 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "form_fields"
-pin "form_preview"
+pin "form_fields", to: "form_fields.js"
+pin "form_preview", to: "form_preview.js"
